7b2e5680c467179afa72336d7ede5f5ae13cdeb5
Ritam Samanta <91908473+RitamSamant@users.noreply.github.com>
2023-08-08 15:22:05 +0530

    Update cars.js
    updated

--- backend/models/cars.js
+++ backend/models/cars.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
 const carDetails = new mongoose.Schema({
-  carModel: { type: String, required: true },
+  brand: { type: String, required: true },
+  name: { type: String, required: true },
   manufacturerName: { type: String, required: true },
-  pricePerDay: { type: Number, required: true },
+  transmission: { type: String, required: true },
+  fueltype: { type: String, required: true },
+  seatNumbers: { type: String, required: true },
+  condition: { type: String, required: true },
+  price: { type: Number, required: true },
+  rentprice: { type: Number, required: true },
   photoLink: { type: String, required: true },
   published: { type: Boolean, required: true },
 });

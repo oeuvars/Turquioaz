14c825d7bf9cfc1da2e219266764388d23de1f80
Ritam Samanta <91908473+wrritam@users.noreply.github.com>
2023-10-16 19:28:44 +0530

    Delete backend directory

--- backend/models/admins.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const adminPanel = new mongoose.Schema({
-  userEmail: { type: String },
-  password: { type: String },
-});
-
-const Admin = mongoose.model("Admin", adminPanel);
-module.exports = Admin;

32c721f601e6a6e4713c9a84fee7496fb3cfba2e
oeuvars <www.anuragniall@gmail.com>
2023-10-25 20:44:19 +0530

    slider fix maybe

--- frontend/src/pages/user/components/Showcase.tsx
+++ frontend/src/pages/user/components/Showcase.tsx
@@ -1,5 +1,5 @@
 import "solid-slider/slider.css";
-import { Slider, SliderProvider } from "solid-slider";
+import { Slider, SliderProvider, createSlider } from "solid-slider";
 import { autoplay } from "solid-slider/plugins/autoplay";
 import car1 from "../../../assets/images/cars/car1.jpg";
 import car2 from "../../../assets/images/cars/car2.jpg";
@@ -35,25 +35,31 @@ const carData = [
 ];
 
 const Showcase: Component = () => {
+  const options = { duration: 1000 };
+  const [slider] = createSlider(
+    { loop: true },
+    autoplay(2000, {
+      pauseOnDrag: true,
+    })
+  );
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+  slider
   return (
     <div class="">
-      <SliderProvider>
-        <Slider options={{ loop: true }} plugins={[autoplay(1500, {})]}>
-          {carData.map((car, index) => (
-            <div class="cursor-pointer relative content__item">
-              <A href="/" class="link link--dia absolute px-3 py-1 italic phone:text-[3.5vh] xs:text-[4.5vh] s:text-[5.5vh] sm:text-[10vw] tablet:text-[10vw] md:text-[9vw] lg:text-[10vw]">
-                {car.title}
-              </A>
-              <img
-                src={car.image}
-                alt="car"
-                class="py-5 rounded-sm filter blur-sm -z-10 phone:h-[30vh] xs:h-[35vh] s:h-[40vh] sm:h-[50vw] tablet:h-[51vw] md:h-[50vw] lg:h-[43vw] w-full object-cover"
-              />
-            </div>
-          ))}
-        </Slider>
-      </SliderProvider>
+      <div use:slider>
+        {carData.map((car, index) => (
+          <div class="cursor-pointer relative content__item">
+            <A href="/" class="link link--dia absolute px-3 py-1 italic phone:text-[3.5vh] xs:text-[4.5vh] s:text-[5.5vh] sm:text-[10vw] tablet:text-[10vw] md:text-[9vw] lg:text-[10vw]">
+              {car.title}
+            </A>
+            <img
+              src={car.image}
+              alt="car"
+              class="py-5 rounded-sm filter blur-sm -z-10 phone:h-[30vh] xs:h-[35vh] s:h-[40vh] sm:h-[50vw] tablet:h-[51vw] md:h-[50vw] lg:h-[43vw] w-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

7904c7c311b11b45c930101f39c84fd46a61fa85
Oeuvars <www.anuragniall@gmail.com>
2024-01-23 21:28:43 +0530

    package.json restored 2

--- backend/dist/helpers/sendOTP.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendOTP = void 0;
-function sendOTP() {
-    const randomNum = Math.random() * 9000;
-    return Math.floor(1000 + randomNum);
-}
-exports.sendOTP = sendOTP;
-//# sourceMappingURL=sendOTP.js.map
